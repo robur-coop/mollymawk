57c020642548e8b6455dbfb1e4dffa4ef50c0fa9
PizieDust <playersrebirth@gmail.com>
2024-07-16 11:53:59 +0100

    move clean string to utils modules

--- utils.ml
+++ utils.ml
@@ -1,4 +1,21 @@
+module Json = struct
+  let clean_string s =
+    (* Remove backslashes and double quotes from the string *)
+    let buffer = Buffer.create (String.length s) in
+    String.iter
+      (fun c ->
+        match c with '\\' -> () | '"' -> () | _ -> Buffer.add_char buffer c)
+      s;
+    Buffer.contents buffer
+end
+
 module Email = struct
   let validate_email email =
-    match Emile.of_string email with Ok _ -> true | Error _ -> false
+    match Emile.of_string (Json.clean_string email) with
+    | Ok _ -> true
+    | Error s ->
+        Logs.err (fun m ->
+            m "Emile-Email-Validation %s"
+              (Format.asprintf "%a\n" Emile.pp_error s));
+        false
 end
