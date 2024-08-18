92cc182cca786740a622cd70f18dab708c1c6304
PizieDust <playersrebirth@gmail.com>
2024-08-18 17:35:21 +0200

    settings index page

---
+++ settings.ml
@@ -0,0 +1,38 @@
+let settings_layout =
+  Tyxml_html.(
+    section
+      ~a:[ a_class [ "col-span-7 p-4 bg-gray-50 my-1" ] ]
+      [
+        div
+          ~a:[ a_class [ "px-3 flex justify-between items-center" ] ]
+          [
+            div
+              [
+                p
+                  ~a:[ a_class [ "font-bold text-gray-700" ] ]
+                  [
+                    txt
+                      ("Configuration");
+                  ];
+              ];
+          ];
+        hr ~a:[ a_class [ "border border-primary-500 my-5" ] ] ();
+        div
+          ~a:[ a_class [ "flex flex-col" ] ]
+          [
+            div
+              ~a:[ a_class [ "-m-1.5 overflow-x-auto" ] ]
+              [
+                div
+                  ~a:
+                    [ a_class [ "p-1.5 min-w-full inline-block align-middle" ] ]
+                  [
+                    div
+                      ~a:[ a_class [ "overflow-hidden" ] ]
+                      [
+
+                      ];
+                  ];
+              ];
+          ];
+      ])
