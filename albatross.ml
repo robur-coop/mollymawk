f24276616e4da61d53106b6b44b11424bbd87606
PizieDust <playersrebirth@gmail.com>
2024-10-01 15:49:38 +0200

    return policies

--- albatross.ml
+++ albatross.ml
@@ -386,7 +386,7 @@ struct
         (* now we tell albatross about it, using a command for throwing it away *)
         (* note that the 'certs' / 'gen_cert' uses the policies for intermediate certificates *)
         query t ~domain (`Unikernel_cmd `Unikernel_info) >|= function
-        | Ok _ -> Ok ()
+        | Ok _ -> Ok (Vmm_trie.collect name t.policies)
         | Error msg ->
             Logs.warn (fun m -> m "error updating policies: %s" msg);
             t.policies <- old_policies;
