5c6e042335fb7155b91e13ae1ea2cb0521b0ddf7
PizieDust <playersrebirth@gmail.com>
2024-11-03 21:30:29 +0100

    increase duration of alerts

--- assets/main.js
+++ assets/main.js
@@ -163,7 +163,7 @@ function postAlert(bg_color, content) {
 		alertContainer.classList.remove("block", `${bg_color}`)
 		alertContainer.classList.add("hidden")
 		alertContainer.removeChild(alert);
-	}, 1600);
+	}, 2500);
 }
 
 async function deployUnikernel() {
