3e9eca0837c7a8552e3234543c3ade90acf1b135
Hannes Mehnert <hannes@mehnert.org>
2024-11-08 15:56:39 +0100

    Update unikernel.ml

--- unikernel.ml
+++ unikernel.ml
@@ -1225,7 +1225,7 @@ struct
          [])
     >>= fun blocks ->
     let policy =
-      Result.to_option (Albatross.policy ~domain:user.name albatross)
+      Result.fold ~ok:Fun.id ~error:(fun _ -> None) (Albatross.policy ~domain:user.name albatross)
     in
     let now = Ptime.v (P.now_d_ps ()) in
     generate_csrf_token store user now reqd >>= function
