d39193737ea7245d1e2c42004b3f731cd2b8b687
Pixie Dust <playersrebirth@gmail.com>
2025-02-18 16:31:55 +0100

    don't check csrf from authenticate when we use multipart-forms

--- unikernel.ml
+++ unikernel.ml
@@ -2145,7 +2145,7 @@ struct
                   (delete_volume !albatross reqd))
         | "/api/volume/create" ->
             check_meth `POST (fun () ->
-                authenticate ~check_token:true ~check_csrf:true ~api_meth:true
+                authenticate ~check_token:true ~check_csrf:false ~api_meth:true
                   store reqd
                   (create_volume !albatross reqd))
         | "/api/volume/download" ->
@@ -2244,7 +2244,7 @@ struct
                   (unikernel_console !albatross unikernel_name reqd))
         | "/api/unikernel/create" ->
             check_meth `POST (fun () ->
-                authenticate ~check_token:true ~check_csrf:true ~api_meth:true
+                authenticate ~check_token:true ~check_csrf:false ~api_meth:true
                   store reqd
                   (unikernel_create !albatross reqd))
         | path when String.starts_with ~prefix:"/unikernel/update/" path ->
