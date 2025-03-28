21418422f0f3da6087702bacd04830e4d3ef51ed
Pixie Dust <playersrebirth@gmail.com>
2025-03-28 12:27:45 +0100

    don't send arguments in json request if toggle is off

--- assets/main.js
+++ assets/main.js
@@ -992,11 +992,11 @@ async function updateUnikernel(job, to_be_updated_unikernel, currently_running_u
 					"currently_running_unikernel": currently_running_unikernel,
 					"unikernel_name": unikernel_name,
 					"unikernel_arguments": argumentsToggle ? JSON.parse(unikernelArguments) : null,
-					"http_liveliness_address": http_address ? http_address : null,
-					"dns_liveliness": {
+					"http_liveliness_address": http_toggle ? (http_address ? http_address : null) : null,
+					"dns_liveliness": dns_toggle ? ({
 						"dns_address": dns_address ? dns_address : null,
 						"dns_name": dns_name ? dns_name : null
-					},
+					}) : null,
 					"molly_csrf": molly_csrf
 				})
 		})
