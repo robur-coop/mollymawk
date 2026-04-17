550e1c01ce0a561fa159585a1deb21ea332ff9a9
PizieDust <playersrebirth@gmail.com>
2026-04-17 01:37:02 +0100

    add advanced config button which contains several settings such as cpu, memory, network and block devices, force creating

--- unikernel_create.ml
+++ unikernel_create.ml
@@ -144,7 +144,11 @@ let unikernel_create_layout ~(user_policy : Vmm_core.Policy.t) unikernels
   let memory_left = user_policy.memory - total_memory_used in
   Tyxml_html.(
     section
-      ~a:[ a_class [ "col-span-7 p-4 bg-gray-50 my-1" ] ]
+      ~a:
+        [
+          a_class [ "col-span-7 p-4 bg-gray-50 my-1" ];
+          Unsafe.string_attrib "x-data" "{ advanced: false }";
+        ]
       [
         div
           ~a:[ a_class [ "px-3 flex justify-between items-center" ] ]
@@ -202,79 +206,9 @@ let unikernel_create_layout ~(user_policy : Vmm_core.Policy.t) unikernels
                       ];
                   ];
               ];
+            (* Solo5 options moved to basic view *)
             div
-              ~a:[ a_class [ "grid grid-cols-3 gap-3" ] ]
-              [
-                cpu_multiselect cpu_usage_count;
-                Utils.increment_or_decrement_ui ~id:"unikernel-memory"
-                  ~max_value:memory_left ~min_value:0 ~default_value:32
-                  ~figure_unit:"MB" ~step:32 ~label':"Memory" ();
-                Utils.increment_or_decrement_ui ~id:"startup-priority"
-                  ~max_value:100 ~min_value:0 ~default_value:50
-                  ~label':"Startup Priority" ();
-              ];
-            (* BHyve Specific Options *)
-            div
-              ~a:
-                [
-                  a_id "bhyve-options";
-                  a_class [ "hidden grid grid-cols-2 gap-3" ];
-                ]
-              [
-                Utils.increment_or_decrement_ui ~id:"numcpus" ~max_value:16
-                  ~min_value:1 ~default_value:1 ~label':"vCPUs" ();
-                div
-                  [
-                    label
-                      ~a:[ a_class [ "block font-medium" ] ]
-                      [ txt "Linux Boot Partition" ];
-                    input
-                      ~a:
-                        [
-                          a_input_type `Text;
-                          a_name "linux_boot_partition";
-                          a_id "linux-boot-partition";
-                          a_placeholder "Optional (e.g. /dev/sda1)";
-                          a_class [ input_classes ];
-                        ]
-                      ();
-                  ];
-              ];
-            hr ();
-            div
-              [
-                label
-                  ~a:[ a_class [ "block font-medium" ] ]
-                  [ txt "Network Interfaces" ];
-                Utils.dynamic_dropdown_form
-                  (Vmm_core.String_set.elements user_policy.bridges)
-                  ~get_label:(fun bridge -> bridge)
-                  ~get_value:(fun bridge -> bridge)
-                  ~id:"network" ();
-              ];
-            hr ();
-            div
-              [
-                label
-                  ~a:[ a_class [ "block font-medium" ] ]
-                  [ txt "Block devices" ];
-                Utils.dynamic_dropdown_form blocks
-                  ~get_label:(fun (name, size, used) ->
-                    Option.value ~default:""
-                      (Option.map Vmm_core.Name.Label.to_string
-                         (Vmm_core.Name.name name))
-                    ^ " - " ^ Int.to_string size ^ "MB (used: "
-                    ^ Bool.to_string used ^ ")")
-                  ~get_value:(fun (name, _, _) ->
-                    Option.value ~default:""
-                      (Option.map Vmm_core.Name.Label.to_string
-                         (Vmm_core.Name.name name)))
-                  ~id:"block" ~manual_entry:true ();
-              ];
-            hr ();
-            (* Solo5 only *)
-            div
-              ~a:[ a_id "solo5-options" ]
+              ~a:[ a_id "solo5-options"; a_class [ "space-y-4 pt-4" ] ]
               [
                 div
                   [
@@ -282,6 +216,7 @@ let unikernel_create_layout ~(user_policy : Vmm_core.Policy.t) unikernels
                       ~a:[ a_class [ "block font-medium" ] ]
                       [ txt "Arguments" ];
                     p
+                      ~a:[ a_class [ "text-sm text-gray-500 mb-1" ] ]
                       [
                         txt "Write arguments seperated by a whitespace e.g ";
                         code [ txt "--ip=127.0.0.1 --port=8180" ];
@@ -289,7 +224,7 @@ let unikernel_create_layout ~(user_policy : Vmm_core.Policy.t) unikernels
                     textarea
                       ~a:
                         [
-                          a_rows 4;
+                          a_rows 2;
                           a_required ();
                           a_name "arguments";
                           a_id "unikernel-arguments";
@@ -297,78 +232,215 @@ let unikernel_create_layout ~(user_policy : Vmm_core.Policy.t) unikernels
                         ]
                       (txt "");
                   ];
-                label
-                  ~a:[ a_class [ "block font-medium" ] ]
-                  [ txt "Unikernel Image Binary*" ];
-                input
+                div
+                  [
+                    label
+                      ~a:[ a_class [ "block font-medium" ] ]
+                      [ txt "Unikernel Image Binary*" ];
+                    input
+                      ~a:
+                        [
+                          a_input_type `File;
+                          a_name "binary";
+                          a_required ();
+                          a_id "unikernel-binary";
+                          a_class [ input_classes; "bg-white" ];
+                        ]
+                      ();
+                  ];
+              ];
+            div
+              ~a:
+                [
+                  a_class
+                    [
+                      "mt-4 bg-white border border-gray-200 rounded-lg \
+                       shadow-sm p-4 text-center mx-auto";
+                    ];
+                ]
+              [
+                h3
+                  ~a:[ a_class [ "text-lg font-bold text-gray-800" ] ]
+                  [ txt "Advanced Configuration" ];
+                p
                   ~a:
                     [
-                      a_input_type `File;
-                      a_name "binary";
-                      a_required ();
-                      a_id "unikernel-binary";
-                      a_class [ input_classes ];
+                      a_class
+                        [ "text-sm text-gray-500 mt-2 mb-4 mx-auto max-w-xl" ];
                     ]
-                  ();
-              ];
-            div
-              ~a:[ a_class [ "" ] ]
-              [
-                label
-                  ~a:[ a_class [ "block font-medium" ] ]
-                  [ txt "Fail Behaviour" ];
-                div
-                  ~a:[ a_class [ "" ] ]
                   [
-                    Utils.switch_button ~switch_id:"restart-on-fail"
-                      ~switch_label:"Restart"
-                      (div
-                         [
-                           label
-                             ~a:
-                               [
-                                 a_class [ "block text-sm font-medium" ];
-                                 a_label_for "restart_description";
-                               ]
-                             [
-                               txt
-                                 "This unikernel will automatically restart on \
-                                  fail";
-                             ];
-                         ]);
+                    txt
+                      "Configure network or block devices, customize hardware \
+                       allocation, or set failure recovery strategies.";
+                  ];
+                button
+                  ~a:
+                    [
+                      a_button_type `Button;
+                      a_class
+                        [
+                          "inline-flex items-center px-4 py-2 border \
+                           border-primary-300 shadow-sm text-sm font-medium \
+                           rounded-md text-primary-700 bg-primary-50 \
+                           hover:bg-primary-100 focus:outline-none \
+                           focus:ring-2 focus:ring-offset-2 \
+                           focus:ring-primary-500";
+                        ];
+                      Unsafe.string_attrib "x-on:click" "advanced = !advanced";
+                    ]
+                  [
+                    span
+                      ~a:
+                        [
+                          Unsafe.string_attrib "x-text"
+                            "advanced ? 'Hide Advanced Settings' : 'Show \
+                             Advanced Settings'";
+                        ]
+                      [];
                   ];
               ];
             div
-              ~a:[ a_class [ "" ] ]
+              ~a:
+                [
+                  a_class
+                    [
+                      "space-y-6 mt-6 p-6 border border-gray-200 rounded-lg \
+                       bg-white";
+                    ];
+                  Unsafe.string_attrib "x-show" "advanced";
+                  Unsafe.string_attrib "x-transition" "";
+                  Unsafe.string_attrib "x-cloak" "";
+                ]
               [
-                label
-                  ~a:[ a_class [ "block font-medium" ] ]
-                  [ txt "Force create" ];
+                div
+                  ~a:[ a_class [ "grid grid-cols-3 gap-3" ] ]
+                  [
+                    cpu_multiselect cpu_usage_count;
+                    Utils.increment_or_decrement_ui ~id:"unikernel-memory"
+                      ~max_value:memory_left ~min_value:0 ~default_value:32
+                      ~figure_unit:"MB" ~step:32 ~label':"Memory" ();
+                    Utils.increment_or_decrement_ui ~id:"startup-priority"
+                      ~max_value:100 ~min_value:0 ~default_value:50
+                      ~label':"Startup Priority" ();
+                  ];
+                (* BHyve Specific Options *)
+                div
+                  ~a:
+                    [
+                      a_id "bhyve-options";
+                      a_class [ "hidden grid grid-cols-2 gap-3" ];
+                    ]
+                  [
+                    Utils.increment_or_decrement_ui ~id:"numcpus" ~max_value:16
+                      ~min_value:1 ~default_value:1 ~label':"vCPUs" ();
+                    div
+                      [
+                        label
+                          ~a:[ a_class [ "block font-medium" ] ]
+                          [ txt "Linux Boot Partition" ];
+                        input
+                          ~a:
+                            [
+                              a_input_type `Text;
+                              a_name "linux_boot_partition";
+                              a_id "linux-boot-partition";
+                              a_placeholder "Optional (e.g. /dev/sda1)";
+                              a_class [ input_classes ];
+                            ]
+                          ();
+                      ];
+                  ];
+                hr ();
+                div
+                  [
+                    label
+                      ~a:[ a_class [ "block font-medium" ] ]
+                      [ txt "Network Interfaces" ];
+                    Utils.dynamic_dropdown_form
+                      (Vmm_core.String_set.elements user_policy.bridges)
+                      ~get_label:(fun bridge -> bridge)
+                      ~get_value:(fun bridge -> bridge)
+                      ~id:"network" ();
+                  ];
+                hr ();
+                div
+                  [
+                    label
+                      ~a:[ a_class [ "block font-medium" ] ]
+                      [ txt "Block devices" ];
+                    Utils.dynamic_dropdown_form blocks
+                      ~get_label:(fun (name, size, used) ->
+                        Option.value ~default:""
+                          (Option.map Vmm_core.Name.Label.to_string
+                             (Vmm_core.Name.name name))
+                        ^ " - " ^ Int.to_string size ^ "MB (used: "
+                        ^ Bool.to_string used ^ ")")
+                      ~get_value:(fun (name, _, _) ->
+                        Option.value ~default:""
+                          (Option.map Vmm_core.Name.Label.to_string
+                             (Vmm_core.Name.name name)))
+                      ~id:"block" ~manual_entry:true ();
+                  ];
+                hr ();
                 div
                   ~a:[ a_class [ "" ] ]
                   [
-                    Utils.switch_button ~initial_state:true
-                      ~switch_id:"force-create"
-                      ~switch_label:"Force create this unikernel"
-                      (div
-                         [
-                           label
-                             ~a:
-                               [
-                                 a_class [ "block text-sm font-medium" ];
-                                 a_label_for "restart_description";
-                               ]
+                    label
+                      ~a:[ a_class [ "block font-medium" ] ]
+                      [ txt "Fail Behaviour" ];
+                    div
+                      ~a:[ a_class [ "" ] ]
+                      [
+                        Utils.switch_button ~switch_id:"restart-on-fail"
+                          ~switch_label:"Restart" ~initial_state:true
+                          (div
                              [
-                               txt
-                                 "If a unikernel exist with this same name, it \
-                                  will be destroyed first and this one \
-                                  created.";
-                             ];
-                         ]);
+                               label
+                                 ~a:
+                                   [
+                                     a_class [ "block text-sm font-medium" ];
+                                     a_label_for "restart_description";
+                                   ]
+                                 [
+                                   txt
+                                     "This unikernel will automatically \
+                                      restart on fail";
+                                 ];
+                             ]);
+                      ];
+                  ];
+                div
+                  ~a:[ a_class [ "" ] ]
+                  [
+                    label
+                      ~a:[ a_class [ "block font-medium" ] ]
+                      [ txt "Force create" ];
+                    div
+                      ~a:[ a_class [ "" ] ]
+                      [
+                        Utils.switch_button ~initial_state:false
+                          ~switch_id:"force-create"
+                          ~switch_label:"Force create this unikernel"
+                          (div
+                             [
+                               label
+                                 ~a:
+                                   [
+                                     a_class [ "block text-sm font-medium" ];
+                                     a_label_for "restart_description";
+                                   ]
+                                 [
+                                   txt
+                                     "If a unikernel exist with this same \
+                                      name, it will be destroyed first and \
+                                      this one created.";
+                                 ];
+                             ]);
+                      ];
                   ];
               ];
             div
-              ~a:[ a_class [ "flex justify-items-center mx-auto w-60" ] ]
+              ~a:[ a_class [ "pt-6 flex justify-items-center mx-auto w-64" ] ]
               [
                 Utils.button_component
                   ~attribs:
