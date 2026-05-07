f14d07f1c0e7bf232d909c03cf3083f7a17a5f91
Reynir Björnsson <reynir@reynir.dk>
2026-05-07 10:17:22 +0200

    Upgrade emile.colombe

--- unikernel.ml
+++ unikernel.ml
@@ -31,8 +31,8 @@ struct
 
   let recipient email =
     match Colombe_emile.to_path email with
-    | Ok path -> [ Colombe.Forward_path.Forward_path path ]
-    | Error (`Msg e) ->
+    | path -> [ Colombe.Forward_path.Forward_path path ]
+    | exception Invalid_argument e ->
         Logs.err (fun m ->
             m "Type conversion failed for %s: %s" (Emile.to_string email) e);
         []
@@ -71,8 +71,8 @@ struct
     in
     let sender =
       match Colombe_emile.to_path email_config.from_email with
-      | Ok path -> Some path
-      | Error _ -> None
+      | path -> Some path
+      | exception Invalid_argument _ -> None
     in
     let streamer =
       let s = Mrmime.Mt.to_stream email in
