7c55d5cd8bb5bbdecf0e2c0becd51f8f45a734f7
PizieDust <playersrebirth@gmail.com>
2026-08-22 21:28:58 +0100

    get the block device once and reuse

--- store.ml
+++ store.ml
@@ -2,32 +2,42 @@ module Make (BLOCK : Mirage_block.S) = struct
   module Stored_data = OneFFS.Make (BLOCK)
   open Lwt.Infix
 
-  let write_data disk data =
-    Stored_data.write disk
-      (Yojson.Basic.to_string
-         (Storage.t_to_json data.Storage.users data.configurations data.email))
-    >|= function
-    | Ok () -> Ok ()
-    | Error we ->
-        Storage.error_msgf "error while writing storage: %a"
-          Stored_data.pp_write_error we
+  let disk = ref None
 
-  let read_data disk =
-    Stored_data.read disk >|= function
-    | Ok (Some s) ->
-        let ( let* ) = Result.bind in
-        let* json = Utils.Json.from_string s in
-        let* t = Storage.t_of_json json in
-        Ok t
-    | Ok None -> Ok ([], [], None)
-    | Error e ->
-        Storage.error_msgf "error while reading storage: %a"
-          Stored_data.pp_error e
+  let write_data data =
+    match !disk with
+    | None -> Lwt.return (Storage.error_msgf "Disk not connected")
+    | Some disk -> (
+        Stored_data.write disk
+          (Yojson.Basic.to_string
+             (Storage.t_to_json data.Storage.users data.configurations
+                data.email))
+        >|= function
+        | Ok () -> Ok ()
+        | Error we ->
+            Storage.error_msgf "error while writing storage: %a"
+              Stored_data.pp_write_error we)
+
+  let read_data () =
+    match !disk with
+    | None -> Lwt.return (Storage.error_msgf "Disk not connected")
+    | Some disk -> (
+        Stored_data.read disk >|= function
+        | Ok (Some s) ->
+            let ( let* ) = Result.bind in
+            let* json = Utils.Json.from_string s in
+            let* t = Storage.t_of_json json in
+            Ok t
+        | Ok None -> Ok ([], [], None)
+        | Error e ->
+            Storage.error_msgf "error while reading storage: %a"
+              Stored_data.pp_error e)
 
   let connect block =
-    Stored_data.connect block >>= fun disk ->
-    read_data disk >|= function
+    Stored_data.connect block >>= fun connected_disk ->
+    disk := Some connected_disk;
+    read_data () >|= function
     | Error _ as e -> e
     | Ok (users, configurations, email) ->
-        Ok (disk, { Storage.users; configurations; email })
+        Ok { Storage.users; configurations; email }
 end
