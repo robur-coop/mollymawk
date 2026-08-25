859c04a9dd2ddb1454acade61b2b5fb81930cf3b
PixieDust <111846546+PizieDust@users.noreply.github.com>
2026-08-25 16:46:23 +0100

    Update tests/storage/tests.ml
    Co-authored-by: Hannes Mehnert <hannes@mehnert.org>

--- tests/storage/tests.ml
+++ tests/storage/tests.ml
@@ -61,7 +61,7 @@ let eq_config (config_1 : Configuration.t list)
       Vmm_core.Name.Label.equal n1 n2
       && Ipaddr.compare si1 si2 = 0
       && sp1 = sp2 && Ptime.equal ua1 ua2
-      && eq_key
+      && String.equal
            (X509.Certificate.fingerprint `SHA256 cer1)
            (X509.Certificate.fingerprint `SHA256 cer2)
       && eq_key
