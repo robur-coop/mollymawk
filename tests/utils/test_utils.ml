a4f06e76826d1ecd7d5374f97a5db787f8d32c61
PizieDust <playersrebirth@gmail.com>
2026-09-15 01:13:28 +0100

    reduce raw dump string

--- tests/utils/test_utils.ml
+++ tests/utils/test_utils.ml
@@ -15,33 +15,6 @@ let raw_dump =
       "cookies": [
         {
           "name": "molly_csrf",
-          "created_at": "2026-04-13 19:29:58-00:00",
-          "value": "MGM5MDZmYjAtMWZjYy00MzE1LTliODEtMjVkNDhiNjFiNWZh",
-          "expires_in": 3600,
-          "uuid": "f208c5b3-3307-417a-81d9-bfcb665155c7",
-          "last_access": "2026-04-13 19:29:58-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-13 19:29:57-00:00",
-          "value": "OTQ2OWRhZGQtODhjZS00NTg4LWEwMjUtZGQ5NDNkNTYwYWI3",
-          "expires_in": 3600,
-          "uuid": "d9a63a9b-ad57-4759-ad05-01b64f9572ba",
-          "last_access": "2026-04-13 19:29:57-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-13 19:29:46-00:00",
-          "value": "YjNhZjI4ZDktODY1NC00NDllLTlkNDYtZjdiMjdmMjBiNTFj",
-          "expires_in": 3600,
-          "uuid": "82bae6d9-75c5-4c7e-9328-f9f582002f36",
-          "last_access": "2026-04-13 19:29:46-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
           "created_at": "2026-04-13 19:29:36-00:00",
           "value": "OGZkZjg4NDYtZGQyNS00MjFjLTg5NGQtZjk3OTAwODllMWM3",
           "expires_in": 3600,
@@ -58,51 +31,6 @@ let raw_dump =
           "last_access": "2025-12-28 18:18:24-00:00",
           "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
         },
-        {
-          "name": "molly_session",
-          "created_at": "2025-12-28 18:15:54-00:00",
-          "value": "OGZjNmY2YzktMWNjYy00NzQ4LWFhMzAtNzk1NmQwNDE4ODYz",
-          "expires_in": 604800,
-          "uuid": "1d0a80e3-323b-4b45-9efd-bad03762e87f",
-          "last_access": "2025-12-28 18:15:54-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_session",
-          "created_at": "2026-04-13 19:25:22-00:00",
-          "value": "MWI4MzYwOWItNmE4MS00ZGUzLWE5NmQtZTJjYWRmNWMzMTcw",
-          "expires_in": 604800,
-          "uuid": "1d0a80e3-323b-4b45-9efd-bad03762e87f",
-          "last_access": "2026-04-13 19:25:22-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-13 19:25:22-00:00",
-          "value": "NTI3MThjYzgtYWVhOC00MDY4LTk0OTgtOWM4ZDIxN2UyODhl",
-          "expires_in": 3600,
-          "uuid": "903bab07-a5bb-4dd5-bf6f-c2cf59e1d8bd",
-          "last_access": "2026-04-13 19:25:22-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-13 19:26:46-00:00",
-          "value": "MGY4NmVhODEtYjVkMC00ODM4LTk3OTEtNmE4Y2QxOWNlOTk5",
-          "expires_in": 3600,
-          "uuid": "703c46ce-4601-42b7-a8d2-8d5d4c9dc8e1",
-          "last_access": "2026-04-13 19:26:46-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-13 19:31:50-00:00",
-          "value": "YjlkYTg3YzgtMDFlZi00MTdjLTgxNGUtOTFmNThhZmM1Mjg3",
-          "expires_in": 3600,
-          "uuid": "c028d665-2b5e-4ec6-bfc5-bdaf75b025e7",
-          "last_access": "2026-04-13 19:31:50-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        }
       ],
       "created_at": "2026-04-13 19:31:50-00:00",
       "updated_at": "2026-04-13 19:29:58-00:00",
@@ -130,42 +58,6 @@ let raw_dump =
           "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
         },
         {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:38:35-00:00",
-          "value": "ODc5ZmQyNTQtMmQ4MS00NDFhLTgxMzQtODNlMWY5NzVkMzMw",
-          "expires_in": 3600,
-          "uuid": "ab2ed4bc-757b-4a62-8edf-2a612f6f8862",
-          "last_access": "2025-12-28 13:38:35-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:38:33-00:00",
-          "value": "OTJhNzUyYzYtNTQ2Ny00ZmNmLWE0NGQtMjNiZTNlNmY0Yjk2",
-          "expires_in": 3600,
-          "uuid": "210c522e-d1cb-4f07-9039-ab1dada57f7b",
-          "last_access": "2025-12-28 13:38:33-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_session",
-          "created_at": "2025-12-08 16:52:44-00:00",
-          "value": "NmUyYjlmZTctOGZiYi00Y2JhLWIzMDctMzk3ZjRiYjRlZWM4",
-          "expires_in": 604800,
-          "uuid": "28538638-0136-43e1-9146-bf7649917a33",
-          "last_access": "2025-12-08 16:52:44-00:00",
-          "user_agent": "PostmanRuntime/7.49.1"
-        },
-        {
-          "name": "molly_session",
-          "created_at": "2025-12-07 10:37:04-00:00",
-          "value": "OTQ0M2QyNTgtNjgyNy00ZjBmLWEwOTYtMGE0OGIwYmI1NTc3",
-          "expires_in": 604800,
-          "uuid": "28538638-0136-43e1-9146-bf7649917a33",
-          "last_access": "2025-12-07 10:37:04-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
-        },
-        {
           "name": "molly_session",
           "created_at": "2025-12-28 13:33:06-00:00",
           "value": "MzNjYTM1ZGItYWVkYS00YTBkLThmMTctMDBiNWM3MWY3MDg2",
@@ -174,60 +66,6 @@ let raw_dump =
           "last_access": "2025-12-28 13:33:06-00:00",
           "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
         },
-        {
-          "name": "molly_session",
-          "created_at": "2025-12-28 13:33:37-00:00",
-          "value": "NzRhYTE0ZGUtNjJlYi00NmI0LWIyYmQtOTg4N2NhYTEzNzg0",
-          "expires_in": 604800,
-          "uuid": "28538638-0136-43e1-9146-bf7649917a33",
-          "last_access": "2025-12-28 13:33:37-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:33:37-00:00",
-          "value": "MDI5Yjc5OTctYzdiOC00MjExLTg4MTAtNGJkYWY4YjgzZThh",
-          "expires_in": 3600,
-          "uuid": "2ca30d0e-4080-431b-a780-f97a4e7b4ec2",
-          "last_access": "2025-12-28 13:33:37-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:33:45-00:00",
-          "value": "YTJiZTQ3OGEtYmJjNC00YTIyLWJmYmMtN2RmNTQ4OWFmMTQw",
-          "expires_in": 3600,
-          "uuid": "c93882b5-5519-4a72-b8d0-8fbd641aa16c",
-          "last_access": "2025-12-28 13:33:45-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:34:10-00:00",
-          "value": "OGIzMThkNzctMjMyNi00ZjQ5LWExNDItMTY5NWIwMjc4YmIw",
-          "expires_in": 3600,
-          "uuid": "80402a15-206d-40ed-9268-87c80d043258",
-          "last_access": "2025-12-28 13:34:10-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:34:14-00:00",
-          "value": "Yzc2ZjIwYzMtMjY4My00N2UxLTg3ODMtY2FiNjA2NGVhNjUy",
-          "expires_in": 3600,
-          "uuid": "42673105-1496-49d0-b23f-00e74d87752f",
-          "last_access": "2025-12-28 13:34:14-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2025-12-28 13:34:17-00:00",
-          "value": "NDhkYmI3YmYtNzIyNC00MDQwLTg0NjktY2NjM2QyYjliMGZl",
-          "expires_in": 3600,
-          "uuid": "7bb5ec3c-51eb-427a-b2d0-cc2640c8928d",
-          "last_access": "2025-12-28 13:34:17-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        }
       ],
       "created_at": "2025-12-28 13:34:17-00:00",
       "updated_at": "2025-12-28 13:38:45-00:00",
@@ -262,42 +100,6 @@ let raw_dump =
           "name": "fobud",
           "usage_count": 68
         },
-        {
-          "token_type": "Bearer",
-          "value": "4ba2912d-be58-4e32-883b-f780cccea150",
-          "expires_in": 2419200,
-          "created_at": "2025-12-05 03:37:22-00:00",
-          "last_access": "2025-12-05 03:37:22-00:00",
-          "name": "Nobur",
-          "usage_count": 0
-        },
-        {
-          "token_type": "Bearer",
-          "value": "31d35fc0-852a-43ee-b604-3fabecab1b83",
-          "expires_in": 2419200,
-          "created_at": "2025-12-05 03:31:33-00:00",
-          "last_access": "2025-12-05 03:31:33-00:00",
-          "name": "Robur",
-          "usage_count": 0
-        },
-        {
-          "token_type": "Bearer",
-          "value": "785b1453-77ed-47fe-901e-f0098404fd4d",
-          "expires_in": 2419200,
-          "created_at": "2025-12-05 03:31:45-00:00",
-          "last_access": "2025-12-05 03:31:45-00:00",
-          "name": "Robur",
-          "usage_count": 48
-        },
-        {
-          "token_type": "Bearer",
-          "value": "9bba7dad-6df2-4339-9ff4-26dccb8c1a4f",
-          "expires_in": 2419200,
-          "created_at": "2025-12-05 04:15:33-00:00",
-          "last_access": "2025-12-05 04:15:33-00:00",
-          "name": "sobur",
-          "usage_count": 2
-        }
       ],
       "cookies": [
         {
@@ -307,69 +109,6 @@ let raw_dump =
           "expires_in": 3600,
           "uuid": "c0c0529c-e905-437a-82f9-4ae3be68856a",
           "last_access": "2026-08-25 23:10:15-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:10:09-00:00",
-          "value": "N2NjY2UzNjQtNTIwYS00M2FmLTliNTYtZWFkNTI5ZDBlZjg3",
-          "expires_in": 3600,
-          "uuid": "194f17b1-8635-4151-a789-d57b7ea083fb",
-          "last_access": "2026-08-25 23:10:09-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:10:05-00:00",
-          "value": "NGYxMDk5OWUtMzEzZC00NjYxLWIwOWEtZmQyMzE1ZWNmYzZk",
-          "expires_in": 3600,
-          "uuid": "44f9f10d-68f4-49f5-bea8-78a127bf7e3c",
-          "last_access": "2026-08-25 23:10:05-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:10:02-00:00",
-          "value": "NzFiNzVhZDctOTcwNi00NTAxLWJmZDAtMDJkMGQ4YWRjZjY2",
-          "expires_in": 3600,
-          "uuid": "112752c3-db96-444c-875e-6a463377317f",
-          "last_access": "2026-08-25 23:10:02-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:10:00-00:00",
-          "value": "NDQyZTBlNjMtYzM1Ny00MmU5LWIwYTAtZmJjNTM3YzczZWYx",
-          "expires_in": 3600,
-          "uuid": "87c554ec-25f2-4a24-9c0a-99696d1ba26d",
-          "last_access": "2026-08-25 23:10:00-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:09:56-00:00",
-          "value": "NTRkNWExNzMtOWZkMC00OTU1LThkMTctZjc1ZWFlODNjODQx",
-          "expires_in": 3600,
-          "uuid": "6916db2d-cd33-4d53-a1ba-4fa8625ee710",
-          "last_access": "2026-08-25 23:09:56-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:09:54-00:00",
-          "value": "N2YzMmI5MWItYjllYi00YzhhLWI5ZDMtNWQ0YjE5YzI0OWNi",
-          "expires_in": 3600,
-          "uuid": "9a0296e3-761b-4aa9-aa17-81a3e313860e",
-          "last_access": "2026-08-25 23:09:54-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-08-25 23:09:50-00:00",
-          "value": "ZWZjNDU1NGMtMDA4NC00ODY1LWFlOTEtYTczNzI3YTFiMTk3",
-          "expires_in": 3600,
-          "uuid": "3b545fc6-0ca2-4217-9a12-6e0dfc853f71",
-          "last_access": "2026-08-25 23:09:50-00:00",
           "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
         },
         {
@@ -436,30 +175,6 @@ let raw_dump =
           },
           "timestamp": "2025-12-08 17:58:24-00:00"
         },
-        {
-          "name": "mello",
-          "job": "hello",
-          "uuid": "6cc1a49d-eb1e-4f46-8471-6452206d1475",
-          "config": {
-            "typ": "solo5",
-            "compressed": false,
-            "fail_behaviour": {
-              "restart": null,
-              "exit_code": [],
-              "all_exit_codes": false
-            },
-            "cpuids": [
-              0
-            ],
-            "memory": 0,
-            "block_devices": [],
-            "network_interfaces": [],
-            "arguments": [],
-            "numcpus": 1,
-            "linux_boot_partition": null
-          },
-          "timestamp": "2025-12-07 23:08:24-00:00"
-        }
       ],
       "scaling_policies": [
         {
@@ -468,68 +183,6 @@ let raw_dump =
           "max_instances": 3
         }
       ]
-    },
-    {
-      "name": "user4",
-      "email": "user4@robur.com",
-      "email_verified": null,
-      "password": "j3I8UShH8DkVKa5U8SHh5+oUXk3bcUrQJvDHos=",
-      "uuid": "ad7bd096-e9e8-4a46-9e7a-ed8b3bf7bdd8",
-      "tokens": [],
-      "cookies": [
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-15 07:44:56-00:00",
-          "value": "ZWQ1NGFjMDktYmQxZC00OTIzLWI1ZjYtMmYxZWRjNWJiNjA4",
-          "expires_in": 3600,
-          "uuid": "acd410b4-ace5-4277-aaac-6e84f2fe59af",
-          "last_access": "2026-04-15 07:44:56-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-15 07:44:01-00:00",
-          "value": "MGYyOWMxNmUtYzcwNy00ZTAyLWFjMmEtYWM3MWU1N2NjZDM2",
-          "expires_in": 3600,
-          "uuid": "b2bd669b-e66a-4583-aa3d-9485c8a68d9d",
-          "last_access": "2026-04-15 07:44:01-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_session",
-          "created_at": "2026-04-14 06:03:25-00:00",
-          "value": "YjQxNGE3ZTEtOGMyYy00NWNjLWIzNTEtN2QzZTc4ZDY5NGI5",
-          "expires_in": 604800,
-          "uuid": "ad7bd096-e9e8-4a46-9e7a-ed8b3bf7bdd8",
-          "last_access": "2026-04-14 06:03:25-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_session",
-          "created_at": "2026-04-14 06:04:18-00:00",
-          "value": "NTM4NWZhZDktMTdkOS00MzQ3LTk3YzYtYTY5N2QzMzc4YTBl",
-          "expires_in": 604800,
-          "uuid": "ad7bd096-e9e8-4a46-9e7a-ed8b3bf7bdd8",
-          "last_access": "2026-04-14 06:04:18-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        },
-        {
-          "name": "molly_csrf",
-          "created_at": "2026-04-14 11:31:41-00:00",
-          "value": "YjZmZmUzNjktZTQyMy00NjFhLWEyNjQtMDRkMDUwNzY5Mjlh",
-          "expires_in": 3600,
-          "uuid": "7b0c8fa4-e4c5-4c9d-9fab-31808236fb7f",
-          "last_access": "2026-04-14 11:31:41-00:00",
-          "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
-        }
-      ],
-      "created_at": "2026-04-14 11:31:41-00:00",
-      "updated_at": "2026-04-15 07:45:00-00:00",
-      "email_verification_uuid": "3e832348-e654-4929-b626-95c53e013d1b",
-      "active": true,
-      "super_user": false,
-      "unikernel_updates": [],
-      "scaling_policies": []
     }
   ],
   "configuration": [
