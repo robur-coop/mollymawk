0f10ae0d45b151148befc11aa9b9aec859c6c707
PizieDust <playersrebirth@gmail.com>
2026-09-17 11:47:23 +0100

    lint

--- tests/api/mock_devices.ml
+++ tests/api/mock_devices.ml
@@ -4,6 +4,7 @@ module Mock_KV : Mirage_kv.RO = struct
   type t = unit
   type error = Mirage_kv.error
   type key = Mirage_kv.Key.t
+
   let pp_error = Mirage_kv.pp_error
   let disconnect _ = Lwt.return_unit
   let exists _ _ = Lwt.return (Ok (Some `Value))
