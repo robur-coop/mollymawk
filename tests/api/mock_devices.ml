88811f09d2b8300e490592a85a99d864fc31308f
PizieDust <playersrebirth@gmail.com>
2026-09-22 08:39:16 +0100

    start mollymawk with the two; success and failure albatross servers

--- tests/api/mock_devices.ml
+++ tests/api/mock_devices.ml
@@ -67,7 +67,8 @@ let make_app_request_handler store =
     }
   in
   let management_domain = Domain_name.of_string_exn "robur.coop" in
-  let config = Lwt_main.run Mock_albatross.albatross_config in
+  let success_config = Lwt_main.run Mock_albatross.success_config in
+  let failure_config = Lwt_main.run Mock_albatross.failure_config in
   let default_policy =
     Vmm_core.Policy.
       {
@@ -96,11 +97,25 @@ let make_app_request_handler store =
     |> Vmm_trie.insert user_path default_policy
     |> fst
   in
-  let instance : Albatross.t =
-    { configuration = config; policies; status = Albatross.Status.Online }
+  let success_instance : Albatross.t =
+    {
+      configuration = success_config;
+      policies;
+      status = Albatross.Status.Online;
+    }
+  in
+  let failure_instance : Albatross.t =
+    {
+      configuration = failure_config;
+      policies;
+      status = Albatross.Status.Online;
+    }
   in
   let albatross_instances = ref App.Label_map.empty in
-  albatross_instances := App.Label_map.singleton config.name instance;
+  albatross_instances :=
+    App.Label_map.empty
+    |> App.Label_map.add success_config.name success_instance
+    |> App.Label_map.add failure_config.name failure_instance;
   let client_addr = (Ipaddr.of_string_exn "127.0.0.1", 8080) in
   let v4 = Ipaddr.V4.Prefix.global in
   let udp =
