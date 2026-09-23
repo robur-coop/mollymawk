ff8e9ed329d7441c2170055c10a6f6569be9fabb
PixieDust <111846546+PizieDust@users.noreply.github.com>
2026-09-23 10:29:46 +0100

    Test for HTML layouts and views (#294)
    * make auth_hdr and cookie_hdr reusable
    
    * use register endpoint to handle users
    
    * first users are admins
    
    * add a root policy and a user policy
    
    * add tests for layouts and html pages

--- tests/api/api_tests.ml
+++ tests/api/api_tests.ml
@@ -5,4 +5,5 @@ let () =
       ("User Administration", Test_admin.tests);
       ("API Tokens", Test_tokens.tests);
       ("Unikernel Operations", Test_unikernel.tests);
+      ("Layouts and Views", Test_views.tests);
     ]
